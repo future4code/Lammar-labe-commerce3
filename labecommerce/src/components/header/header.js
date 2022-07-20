508144d7f1dba0698375ca8cbe64185f0db18533
unknown <vmotta95@gmail.com>
2022-07-19 21:35:41 -0300

    logo-inicio-card

---
+++ labecommerce/src/components/header/header.js
@@ -0,0 +1,13 @@
+import { HeaderContainer } from './StyledHeader'
+import logo from "../../img/labecommerce-logo.png"
+import React from 'react'
+
+
+ export function Header() {
+   return (
+   <HeaderContainer>
+     <img src={logo}/>
+   </HeaderContainer>
+   )
+}
+
---
+++ labecommerce/src/components/products/styledproducts.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const CardContainer = styled.div`
+   background-color: lavender;
+   margin: 5px;
+   padding: 0 15px;
+   border-radius: 5px;
+   width: 250px;
+`
